a3544a489a1e0be52a730399d50a2ef24f3bee95
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-10 18:10:15 -0500

    Paso1: 1h terminado y ya hecho la estructura para hacerlo con React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+// import { useState } from "react";
 import "./style.css";
 
 function App() {
+  // const [useState, setNewItem]=useState("")
   return (
     <>
       <form className="new-item-form">
