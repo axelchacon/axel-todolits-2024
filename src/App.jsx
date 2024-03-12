bc4e58f8ed35b05f7c2083709e4dc6c327c3ed2e
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-12 16:30:20 -0500

    Paso3.3: Components , termianando lo de NewTodoForm junto con la instalación de un npm para prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
 import "./style.css";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
-  const [newItem, setNewItem] = useState(""); ///variable de estado para controlar el input
+  // const [newItem, setNewItem] = useState(""); ///variable de estado para controlar el input
   const [todos, setTodos] = useState([]); ///variable de estado para controlar la lista de las tareas
-
-  function handleChange(event) {
-    setNewItem(event.target.value); ///valor string donde la variable de estado es string;
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Submit");
-    if (newItem === "") return;
-    ///del setTodos es un valor array o es un estado tipo array por lo que se maneja especial mente con copia usando los pntos "....."
+  function addTodo(title) {
     setTodos([
       ...todos,
       {
         id: crypto.randomUUID(),
-        title: newItem,
+        title: title,
         completed: false,
       },
     ]);
-    setNewItem(""); ///esto permite que luego de escibir en el input, ese valor no se queda en el cuadro del input al moento de terminar de aplastar ADD sino que al terminar de apalstar queda vacío si guardarse el valor último escrito agregado
   }
+  // function handleChange(event) {
+  //   setNewItem(event.target.value); ///valor string donde la variable de estado es string;
+  // }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   console.log("Submit");
+  //   if (newItem === "") return;
+  //   ///del setTodos es un valor array o es un estado tipo array por lo que se maneja especial mente con copia usando los pntos "....."
+  //   setTodos([
+  //     ...todos,
+  //     {
+  //       id: crypto.randomUUID(),
+  //       title: newItem,
+  //       completed: false,
+  //     },
+  //   ]);
+  //   setNewItem(""); ///esto permite que luego de escibir en el input, ese valor no se queda en el cuadro del input al moento de terminar de aplastar ADD sino que al terminar de apalstar queda vacío si guardarse el valor último escrito agregado
+  // }
 
   //// toggleTodo forma1 /////
   // function toggleTodo(id, checked) {
@@ -55,19 +65,8 @@ function App() {
   console.log(todos);
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            type="text"
-            id="item"
-            value={newItem}
-            onChange={handleChange}
-          />
-          {/* <p>{newItem}</p> */}
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo} />
+
       <h1 className="header">Todo List</h1>
       <ul className="list">
         {/* esto retorna algo si las condiciones sí o sí son true */}
